2f9fe1cb983a5fd22c9560369e7d8fbf841b04be
zansangeeth <zansangeeth17@gmail.com>
2022-09-03 12:37:07 +0530

    Initial commit

---
+++ app/src/main/java/com/zasa/retrofitcoroutines/Constants.kt
@@ -0,0 +1,9 @@
+package com.zasa.retrofitcoroutines
+
+/**
+ **@Project -> RetrofitCoroutines
+ **@Author -> Sangeeth on 9/3/2022
+ */
+object Constants {
+    const val BASE_URL = "https://jsonplaceholder.typicode.com/"
+}
